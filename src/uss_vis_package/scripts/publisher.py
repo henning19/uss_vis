2df11a4f6939b2ffbc97eb948c6fe1994e10335a
henning19 <hohe03@gmail.com>
2024-04-10 11:55:19 +0200

    First Vis is running

--- src/uss_vis_package/scripts/publisher.py
+++ src/uss_vis_package/scripts/publisher.py
@@ -3,24 +3,31 @@
 import rospy
 #from data_type_init import Coordinates
 from std_msgs.msg import String
+from geometry_msgs.msg import PointStamped 
 from geometry_msgs.msg import Point
 
 
-
 def publisher():
-    pub = rospy.Publisher('chatter', Point, queue_size=10)
+    pub = rospy.Publisher('chatter', PointStamped, queue_size=10)
     rospy.init_node('publisher', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     i=0.0
     while not rospy.is_shutdown():
-        i=i+0.1
-        x=10+i
-        y=10-i
-        dummy_cor=Point(x,y,0)
+        i=i+1
+        x_i=-5+i
+        y_i=-5+i
+        dummy_cor=PointStamped()
+        dummy_cor.point=Point(x=x_i, y=y_i, z=0)
+        #dummy_cor.point.y=y_i
+        #dummy_cor.point.z=0.0
+        dummy_cor.header.stamp=rospy.Time.now()
+        dummy_cor.header.frame_id="dummy_cor"
         if i==10:
+            x_i=-5
+            y_i=-5
             i=0
         pub.publish(dummy_cor)
-        rospy.loginfo("Published coordinates: x=%f y=%f",x,y)
+        rospy.loginfo("Published coordinates: x=%f y=%f",x_i,y_i)
         rate.sleep()
 
 if __name__ == '__main__':
